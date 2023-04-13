b156408ddce52c7b5659b381655820801ca4a7c1
echashin <echashin@gmail.com>
2023-04-13 14:30:54 +0300

    pageable as extra model

--- src/decorators/crud-controller.decorator.ts
+++ src/decorators/crud-controller.decorator.ts
@@ -1,23 +1,25 @@
 import {applyDecorators, Controller, Type} from "@nestjs/common";
-import {ApiTags} from "@nestjs/swagger";
+import {ApiExtraModels, ApiTags} from "@nestjs/swagger";
 import {paramCase} from "change-case";
+import {Pageable} from "../dto";
 
 
 export const CrudController: any = <TModel extends Type<any>>(
-  model: TModel
+    model: TModel
 ) => {
-  const name: string = `crud-${paramCase(model.name).replace(
-    /-entity$/,
-    ""
-  )}`;
-  return applyDecorators(
-    // ApiHeader({
-    //   name: "lang",
-    //   required: true,
-    //   enum: LangCodeEnum,
-    //   example: LangCodeEnum.en,
-    // }),
-    ApiTags(name),
-    Controller(name)
-  );
+    const name: string = `crud-${paramCase(model.name).replace(
+        /-entity$/,
+        ""
+    )}`;
+    return applyDecorators(
+        // ApiHeader({
+        //   name: "lang",
+        //   required: true,
+        //   enum: LangCodeEnum,
+        //   example: LangCodeEnum.en,
+        // }),
+        ApiExtraModels(Pageable),
+        ApiTags(name),
+        Controller(name)
+    );
 };
